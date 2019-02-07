ad4ada5ee5d0f735426be3879180dfc496cb2651
emilie bialais <emiliebialais@MacBookAir.local>
2019-02-07 15:18:32 +0100

    Jeu 15:17 modif objet

--- js/script.js
+++ js/script.js
@@ -1,48 +1,61 @@
 window.onload = function(){
 
-    let score = 0;
-    var multiplicateur =1 ;
 
+    // les objets
     let pokemon = {
-        clicking : function() {
-                    score++;
-                    console.log (score)
-                    let txt = "Votre score est de " + score + " pikachu !"
-                    console.log(txt)
-                    aff.innerText = txt
-        },
-       
-        
-
+        score : 0,
+        multiplicateur  :1 ,
 
     }
 
     let bonus = {
         active : "false",
-        activate : function() {
-            // stackoverflow propose
-            // document.getElementById('id').style.pointerEvents = 'none';
-            // document.getElementById('id').style.pointerEvents = 'auto'; 
-            // mais rendra aussi idispo le over à mon avis
-        },
-        augmenterMultiplicateur : function(){
-            multiplicateur++
-            console.log(multiplicateur);
-        }
 
     }
 
+    // les variables
+
     let cli = document.getElementById('clic');
     let aff = document.getElementById('affichage');
-    let multi= document.getElementById('multiplier'); 
+    let multi = document.getElementById('multiplier'); 
+    let boost = document.getElementById('boost')
 
+    
+    //les fonctions
+
+
+    function clicking () {
+        pokemon.score++;
+        console.log (pokemon.score)
+        let txt = "Votre score est de " + pokemon.score + " pikachu !"
+        aff.innerText = txt
+        return pokemon.score
+    };
+
+    function activate () {
+        // stackoverflow propose
+        // document.getElementById('id').style.pointerEvents = 'none';
+        // document.getElementById('id').style.pointerEvents = 'auto'; 
+        // mais rendra aussi indispo le over à mon avis
+    };
+
+    function augmenterMultiplicateur () {
+        pokemon.multiplicateur++
+        console.log(pokemon.multiplicateur);
+    };
+
+    function booster () {
+        setInterval()
+    };
 
     // fonction affiche le score à chaque clic
-    cli.addEventListener('click', pokemon.clicking)
+    cli.addEventListener('click', clicking)
 
     // fonction pour compter le nombre de multiplicateur utilisé
-    multi.addEventListener("click", bonus.augmenterMultiplicateur);
+    multi.addEventListener("click", augmenterMultiplicateur);
 
 
+    // fonction pour lancer le boost
+    boost.addEventListener("click", booster);
 }
 
