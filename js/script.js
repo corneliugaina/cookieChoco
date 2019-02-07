3512a9741bf72b7bb69770ffd79b637ef5d28b57
emilie bialais <emiliebialais@MacBookAir.local>
2019-02-07 12:09:42 +0100

    F et E

--- js/script.js
+++ js/script.js
@@ -1,46 +1,45 @@
- window.onload = function(){
+window.onload = function(){
 
-<<<<<<< HEAD
-     let cli = document.getElementById('clic');
-     let aff = document.getElementById('affichage');
-     let score =0;
-=======
     let score = 0;
+    var multiplicateur =1 ;
+
     let pokemon = {
-        clicking : function (){
+        clicking : function() {
                     score++;
                     console.log (score)
                     let txt = "Votre score est de " + score + " pikachu !"
                     console.log(txt)
                     aff.innerText = txt
         },
-        bonus {
+        bonus = {
+            active : "false",
+            activate : function() {
+                // stackoverflow propose
+                // document.getElementById('id').style.pointerEvents = 'none';
+                // document.getElementById('id').style.pointerEvents = 'auto'; 
+                // mais rendra aussi idispo le over à mon avis
+            },
+            augmenterMultiplicateur : function(){
+                multiplicateur++
+                console.log(multiplicateur);
+            }
 
         },
-        multiCount : 0,
+        
 
 
     }
 
     let cli = document.getElementById('clic');
     let aff = document.getElementById('affichage');
->>>>>>> devEmi
+    let multi= document.getElementById('multiplier'); 
 
 
+    // fonction affiche le score à chaque clic
+    cli.addEventListener('click', pokemon.clicking)
 
-//exo 4
-
-
-    let multi= document.getElementById('multiplier');   
+    // fonction pour compter le nombre de multiplicateur utilisé
     multi.addEventListener("click", augmenterMultiplicateur);
 
-    var multiplicateur=1;
-    function augmenterMultiplicateur(){
-        
-        multiplicateur++
-        console.log(multiplicateur);
-        
-    }
-    
 
 }
