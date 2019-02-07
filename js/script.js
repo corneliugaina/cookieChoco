83115efbb5122ed9f89439ec47aeea4b5378c4b0
frmassar <f_massart@hotmail.com>
2019-02-07 12:02:41 +0100

    hello2

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
  window.onload = function(){
 
-     let cli = document.getElementById('clic');
-     let aff = document.getElementById('affichage');
-     let score =0;
+    let cli = document.getElementById('clic');
+    let aff = document.getElementById('affichage');
+    let score =0;
 
 
 
-//exo 4
+    //exo 4
 
 
-    let multi= document.getElementById('multiplier');   
-    multi.addEventListener("click", augmenterMultiplicateur);
+let multi= document.getElementById('multiplier');   
+multi.addEventListener("click", augmenterMultiplicateur);
 
-    var multiplicateur=1;
-    function augmenterMultiplicateur(){
-        
-        multiplicateur++
-        console.log(multiplicateur);
-        
-    }
+var multiplicateur=1;
+function augmenterMultiplicateur(){
     
+    multiplicateur++
+    console.log(multiplicateur);
+    
+}
+
 
 }
