e4af90c4b49fca41c472db35766a636ca08a81e8
emilie bialais <emiliebialais@MacBookAir.local>
2019-02-12 16:23:14 +0100

    bonus counter

--- js/script.js
+++ js/script.js
@@ -23,9 +23,9 @@ window.onload = function(){
         },
         bonus : { 
             active : false,
-            prixMulti : 50,
-            prixAutoclic : 500,
-            prixBoost : 5000,
+            prixMulti : 5,
+            prixAutoclic : 6,
+            prixBoost : 7,
             boostCount: 1,
             tclic : Number.MAX_SAFE_INTEGER,
             tBoosterOn : 0,
@@ -45,7 +45,7 @@ window.onload = function(){
     //les fonctions
 
     function affScore () {
-        let txt = pokemon.score + " pikachu !"
+        let txt = pokemon.score + " pikachu(s) !"
         pokemon.button.aff.innerText = txt
     };
 
