28b9ed6d741234529c3bf82f857faf5f0ae4c928
emilie bialais <emiliebialais@MacBookAir.local>
2019-02-13 16:58:45 +0100

    messages

--- js/script.js
+++ js/script.js
@@ -25,9 +25,9 @@ window.onload = function(){
         },
         bonus : { 
             active : false,
-            prixMulti : 50,
-            prixAutoclic : 500,
-            prixBoost : 5000,
+            prixMulti : 25,
+            prixAutoclic : 250,
+            prixBoost : 2500,
             boostCount: 1,
             tclic : Number.MAX_SAFE_INTEGER,
             tBoosterOn : 0,
@@ -201,43 +201,43 @@ window.onload = function(){
             pokemon.affQuoteTxt = 'Level 2 : Une foule de Pikachus enfilent leur gilets jaunes et cassent le quartier !';
             pokemon.button.affQuote.innerText = pokemon.affQuoteTxt;
         } else if (pokemon.score < 800) {
-            pokemon.affQuoteTxt = 'Level 3 : Les Pikachus creent un syndicat et bloquent le ring de Bruxelles !';
+            pokemon.affQuoteTxt = 'Level 3 : Tes Pikachus creent un syndicat et bloquent le ring de Bruxelles !';
             pokemon.button.affQuote.innerText = pokemon.affQuoteTxt;
         } else if (pokemon.score < 2000) {
-            pokemon.affQuoteTxt = 'Level 4 : Les Pikachus passent les communes a facilites et prennent en tenaille la region walonne/flamande !';
+            pokemon.affQuoteTxt = 'Level 4 : Tes Pikachus passent les communes à facilité et prennent en tenaille la région flamande !';
             pokemon.button.affQuote.innerText = pokemon.affQuoteTxt;
-        } else if (pokemon.score < 4000) {
-            pokemon.affQuoteTxt = 'Level 5 : Les Pikachus prennent d\'assaut le Benelux, les luxembourgeois creent la crypto Pikacoin a leur efigie ! !';
+        } else if (pokemon.score < 3000) {
+            pokemon.affQuoteTxt = 'Level 5 : Tes Pikachus prennent d\'assaut le Benelux, les luxembourgeois créent la crypto Pikacoin à leur éfigie ! !';
             pokemon.button.affQuote.innerText = pokemon.affQuoteTxt;
-        } else if (pokemon.score < 8000) {
-            pokemon.affQuoteTxt = 'Level 6 : La France est prise pour cible, les Pikachus font concurrence aux Lapins Cretins !'; 
+        } else if (pokemon.score < 6000) {
+            pokemon.affQuoteTxt = 'Level 6 : La France est prise pour cible, tes Pikachus font concurrence aux Lapins Crétins !'; 
             pokemon.button.affQuote.innerText = pokemon.affQuoteTxt;
-        } else if (pokemon.score < 16000) {
-            pokemon.affQuoteTxt = 'Level 7 : Les Pikachus s\'organisent et commencent a puiser l\'energie des centrales nucleaires !'; 
+        } else if (pokemon.score < 10000) {
+            pokemon.affQuoteTxt = 'Level 7 : Tes Pikachus s\'organisent et commencent à puiser l\'énergie des centrales nucléaires !'; 
+            pokemon.button.affQuote.innerText = pokemon.affQuoteTxt;
+        } else if (pokemon.score < 20000) {
+            pokemon.affQuoteTxt = 'Level 8 : Invasion de Pikachus dans toute l\'Europe, ils donnent de l\'énergie renouvelable !'; 
             pokemon.button.affQuote.innerText = pokemon.affQuoteTxt;
         } else if (pokemon.score < 30000) {
-            pokemon.affQuoteTxt = 'Level 8 : Invasion des Pikachus dans toute l\'Europe, ils donnent de l\'energie renouvelable !'; 
+            pokemon.affQuoteTxt = 'Level 9 : Tes Pikachus créent leur nouvel ordre mondial et déclarent le Nouvel Age Electrique !'; 
             pokemon.button.affQuote.innerText = pokemon.affQuoteTxt;
-        } else if (pokemon.score < 70000) {
-            pokemon.affQuoteTxt = 'Level 9 : Les Pikachus creent leur nouvel ordre mondial et declarent le Nouvel Age Electrique !'; 
+        } else if (pokemon.score < 40000) {
+            pokemon.affQuoteTxt = 'Level 10 : Le monde entier est envahi par tes Pikachus !';
             pokemon.button.affQuote.innerText = pokemon.affQuoteTxt;
-        } else if (pokemon.score < 140000) {
-            pokemon.affQuoteTxt = 'Level 10 : Le monde entier est envahi par des Pikachus !';
+        } else if (pokemon.score < 50000) {
+            pokemon.affQuoteTxt = 'Level 11 : Les fusees de la NASA sont alimentées par tes Pikachu, tes pokemons se lancent à la conquête de la Lune !';
             pokemon.button.affQuote.innerText = pokemon.affQuoteTxt;
-        } else if (pokemon.score < 300000) {
-            pokemon.affQuoteTxt = 'Level 11 : Les fusees de la NASA sont alimentees par des Pikachu, les pokemons se lancent a la conquete de la Lune !';
+        } else if (pokemon.score < 100000) {
+            pokemon.affQuoteTxt = 'Level 12 : Tes Pikachus construisent des colonies sur Mars et Elon Musk installe un Pikachu dans chaque Tesla !';
+            pokemon.button.affQuote.innerText = pokemon.affQuoteTxt;
+        } else if (pokemon.score < 200000) {
+            pokemon.affQuoteTxt = 'Level 13 : Le systeme solaire est renommé en \'Système Pikachu\' !';
             pokemon.button.affQuote.innerText = pokemon.affQuoteTxt;
         } else if (pokemon.score < 500000) {
-            pokemon.affQuoteTxt = 'Level 12 : Les Pikachus construisent des colonies sur Mars et Elon Musk installe un Pikachu dans chaque Tesla !';
-            pokemon.button.affQuote.innerText = pokemon.affQuoteTxt;
-        } else if (pokemon.score < 1000000) {
-            pokemon.affQuoteTxt = 'Level 13 : Le systeme solaire est renomme en \'Systeme Pikachu\' !';
-            pokemon.button.affQuote.innerText = pokemon.affQuoteTxt;
-        } else if (pokemon.score < 2000000) {
-            pokemon.affQuoteTxt = 'Level 14 : Les Pikachu voyagent a la vitesse de la lumiere et se mettent a la conquete de la Voie Lactee !';
+            pokemon.affQuoteTxt = 'Level 14 : Tes Pikachu voyagent à la vitesse de la lumière et se mettent à la conquête de la Voie Lactée !';
             pokemon.button.affQuote.innerText = pokemon.affQuoteTxt;
         } else {
-            pokemon.affQuoteTxt = 'Level 15 : Les Pikachu mettent au point une machine de voyage spatio-temporelle et envahissent les dimensions paralleles !';
+            pokemon.affQuoteTxt = 'Level 15 : Tes Pikachu mettent au point une machine de voyage spatio-temporelle et envahissent les dimensions parallèles !';
             pokemon.button.affQuote.innerText = pokemon.affQuoteTxt;
         } 
     }
